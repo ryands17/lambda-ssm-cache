90e359ea2f38d395efd4c7b48198414cb7406405
Ryan <dsouza.ryan.11@gmail.com>
2020-11-24 19:35:24 +0530

    add tests

--- test/lambda-ssm.test.ts
+++ test/lambda-ssm.test.ts
@@ -1,18 +1,63 @@
-// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert'
-// import * as cdk from '@aws-cdk/core'
-// import * as LambdaSsm from '../lib/lambda-ssm-stack'
+import {
+  deepObjectLike,
+  expect as expectCDK,
+  haveResourceLike,
+} from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import { LambdaSsmStack } from '../lib/lambda-ssm-stack'
 
-// test('Empty Stack', () => {
-//   const app = new cdk.App()
-//   // WHEN
-//   const stack = new LambdaSsm.LambdaSsmStack(app, 'MyTestStack')
-//   // THEN
-//   expectCDK(stack).to(
-//     matchTemplate(
-//       {
-//         Resources: {},
-//       },
-//       MatchStyle.EXACT
-//     )
-//   )
-// })
+test('The SSM Parameter with the correct values', () => {
+  const stack = createStack()
+  expectCDK(stack).to(
+    haveResourceLike('AWS::SSM::Parameter', {
+      Type: 'String',
+      Value: 'value1',
+      Name: '/dev/key1',
+    })
+  )
+})
+
+test('The Lambda function with the correct parameters and log retention', () => {
+  const stack = createStack()
+  expectCDK(stack).to(
+    haveResourceLike('AWS::Lambda::Function', {
+      Code: {
+        S3Bucket: {},
+      },
+      Handler: 'dist/index.handler',
+      Role: {},
+      Runtime: 'nodejs12.x',
+      MemorySize: 512,
+    })
+  )
+
+  expectCDK(stack).to(haveResourceLike('Custom::LogRetention'))
+})
+
+test('The IAM Role and Policy for Lambda access to Parameter Store', () => {
+  const stack = createStack()
+  expectCDK(stack).to(haveResourceLike('AWS::IAM::Role'))
+
+  expectCDK(stack).to(
+    haveResourceLike(
+      'AWS::IAM::Policy',
+      deepObjectLike({
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: 'ssm:GetParametersByPath',
+              Effect: 'Allow',
+              Resource: {},
+            },
+          ],
+        },
+      })
+    )
+  )
+})
+
+const createStack = () => {
+  const app = new cdk.App()
+  const stack = new LambdaSsmStack(app, 'SSMLambdaStack')
+  return stack
+}
